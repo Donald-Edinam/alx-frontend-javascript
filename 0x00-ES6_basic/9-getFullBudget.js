a5625651cdcde86b4a2b40ef301bb8f51eb73dc9
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-05-28 17:34:42 +0000

    Full budget object added

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,23 @@
+//Rewrite getFullBudgetObject to use ES6 method properties in the fullBudget object
+
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    // getIncomeInDollars: function (income) {
+    //   return `$${income}`;
+    // },
+    // getIncomeInEuros: function (income) {
+    //   return `${income} euros`;
+    // },
+
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`
+
+
+  };
+
+  return fullBudget;
+}
