010bae93e894c89e78959c770f38a5419b5008be
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-05-29 20:54:46 +0000

    My promise will run

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,14 +1,16 @@
-export default function (boolean) {
+export default function getFullResponseFromAPI(success){
     return new Promise((resolve, reject) => {
-      const object = {
-        status: 200,
-        body: 'Success',
-      };
-  
-      if (boolean === true) {
-        resolve(object);
-      } else {
-        reject(Error('The fake API is not working currently'));
-      }
-    });
-  }
+        
+        success_object = {
+            status: 200, 
+            body: "Success"
+        }
+
+        if(success === true){
+            resolve(success_object)
+        }
+        else{
+            reject("This fake API is not working currently");
+        }
+    })
+}
