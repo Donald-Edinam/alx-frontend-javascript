d0f6fadac7230aa4b493ea274c7d16af56648675
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-05-30 00:59:16 +0000

    Promise want o fail

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { upLoadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup(){
-    Promise.all([
-        upLoadPhoto(),
-        createUser(),
-    ])
-    .then((value) =>  console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`))
-    .catch((error) => console.log("Signup system offline", error))
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((value) => {
+      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
+    })
+
+    .catch(() => console.log('Signup system offline'));
 }
