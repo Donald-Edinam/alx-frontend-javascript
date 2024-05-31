685a31cff0400b863ede3187af108b6f4edbb76d
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-05-31 23:24:38 +0000

    Promise resolved

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((value) => {
-      console.log(`${value[0].body} ${value[1].firstName} ${value[1].lastName}`);
-    })
+  const photo = uploadPhoto();
+  const user = createUser();
 
-    .catch(() => console.log('Signup system offline'));
+  return Promise.all([photo, user]).then((res) => {
+    console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+  })
+    .catch(() => { console.log('Signup system offline'); });
 }
