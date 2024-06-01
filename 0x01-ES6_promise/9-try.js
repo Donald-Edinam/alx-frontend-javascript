e8d634b5bb112c33175727435f528badb4ff0585
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-01 00:26:13 +0000

    Error found and fixed

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,14 +1,12 @@
 export default function guardrail(mathFunction) {
     const queue = [];
-    try{
-        const value = mathFunction();
-        queue.push(value)
+    try {
+      const result = mathFunction();
+      queue.push(result);
+    } catch (error) {
+      queue.push(`Error: ${error.message}`);
+    } finally {
+      queue.push('Guardrail was processed');
     }
-    catch (error){
-        queue.push(`Error: ${error.message}`)
-    }finally{
-        queue.push(" Guardrail was processed")
-    }
-    
     return queue;
-}
+  }
