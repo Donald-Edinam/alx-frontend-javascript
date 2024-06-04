a780da4bb36a40de5d572c7b5d9a920d05e06da0
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-04 22:41:49 +0000

    Reverse

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,10 +1,6 @@
-export default function getListStudentIds(obj) {
-    const arr = Array.isArray(obj);
-    
-    if(arr){
-        return obj.map((list) => list.id)
+export default function getListStudentIds(list) {
+    if (Array.isArray(list)) {
+      return list.map((obj) => obj.id);
     }
-    else{
-        return [];
-    }
-}
+    return [];
+  }
