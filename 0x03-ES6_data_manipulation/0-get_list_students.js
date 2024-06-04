3f21befccbbc0e7314e3ecce775139d4fd81fb83
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-04 07:30:24 +0000

    Morning breeze

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,9 @@
+export default function getListStudents() {
+    const studentLists = [
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Colombia' },
+        { id: 3, firstName: 'Serena', location: 'San Francisco' },
+    ]
+
+    return studentLists
+}
