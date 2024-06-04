ad9d1e76cee23d418bb8ac04ed26c470b9722b0d
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-04 22:42:58 +0000

    Realizing...

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,10 @@
-export default function getListStudentIds(list) {
-    if (Array.isArray(list)) {
-      return list.map((obj) => obj.id);
+export default function getListStudentIds(obj) {
+    const arr = Array.isArray(obj);
+    
+    if(arr){
+        return obj.map((list) => list.id)
     }
-    return [];
-  }
+    else{
+        return [];
+    }
+}
