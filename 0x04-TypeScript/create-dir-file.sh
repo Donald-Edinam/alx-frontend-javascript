d95378f2d065ef0c6172ed89e17ae31aa925b0ec
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-06 18:06:13 +0000

    Files added

---
+++ 0x04-TypeScript/create-dir-file.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Create directories and files for task 0
+mkdir -p task_0/js
+touch task_0/js/main.ts
+touch task_0/package.json
+touch task_0/.eslintrc.js
+touch task_0/tsconfig.json
+touch task_0/webpack.config.js
+
+# Create directories and files for task 1
+mkdir -p task_1/js
+touch task_1/js/main.ts
+touch task_1/package.json
+touch task_1/tsconfig.json
+touch task_1/webpack.config.js
+
+# Create directories and files for task 2
+mkdir -p task_2/js
+touch task_2/js/main.ts
+touch task_2/package.json
+touch task_2/tsconfig.json
+touch task_2/webpack.config.js
+
+# Create directories and files for task 3
+mkdir -p task_3/js
+touch task_3/js/main.ts
+touch task_3/js/interface.ts
+touch task_3/js/crud.d.ts
+touch task_3/package.json
+touch task_3/tsconfig.json
+touch task_3/webpack.config.js
+
+# Create directories and files for task 4
+mkdir -p task_4/js/subjects
+touch task_4/js/subjects/Cpp.ts
+touch task_4/js/subjects/Java.ts
+touch task_4/js/subjects/React.ts
+touch task_4/js/subjects/Subject.ts
+touch task_4/js/subjects/Teacher.ts
+touch task_4/js/main.ts
+touch task_4/package.json
+touch task_4/tsconfig.json
+
+# Create directories and files for task 5
+mkdir -p task_5/js
+touch task_5/js/main.ts
+touch task_5/package.json
+touch task_5/tsconfig.json
+touch task_5/webpack.config.js
+
+# Confirmation message
+echo "Directory structure and files have been created."
