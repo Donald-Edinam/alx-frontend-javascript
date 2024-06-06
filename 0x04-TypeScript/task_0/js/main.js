2a8deccacb1042274e3abca45322afe4eaf9eb4a
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-06 20:06:04 +0000

    Done with task 0

--- 0x04-TypeScript/task_0/js/main.js
+++ 0x04-TypeScript/task_0/js/main.js
@@ -1,50 +1,34 @@
-interface Student{
-    firstName: string,
-    lastName: string,
-    age: number,
-    location: string
-}
-
-const student1: Student = {
+var student1 = {
     firstName: "Kofi",
     lastName: "Manu",
     age: 23,
     location: "Kumasi"
-}
-
-const student2: Student = {
+};
+var student2 = {
     firstName: "Ama",
     lastName: "Bonsu",
     age: 32,
     location: "Pankrono"
-}
-
-const studentsList = [student1, student2]
-
-const table = document.createElement('table');
-const tbody = document.createElement('tbody');
-
+};
+var studentsList = [student1, student2];
+var table = document.createElement('table');
+var tbody = document.createElement('tbody');
 table.style.background = 'coral';
 table.appendChild(tbody);
-
-studentsList.forEach((student: Student): void => {
-    const row = document.createElement('tr');
-    const parentCell = document.createElement('td');
-    const childCell = document.createElement('tr');
-
+studentsList.forEach(function (student) {
+    var row = document.createElement('tr');
+    var parentCell = document.createElement('td');
+    var childCell = document.createElement('tr');
     parentCell.textContent = student.firstName;
     childCell.textContent = student.lastName;
-    
     // Style the table
     parentCell.style.border = "1px solid coral";
     childCell.style.border = "1px solid coral";
     parentCell.style.padding = "5px";
     childCell.style.padding = "5px";
-
     // Add row to table
     row.appendChild(parentCell);
-    row.appendChild(childCell)
-
+    row.appendChild(childCell);
     // Add table to body
-    document.body.appendChild(table)
-})
+    document.body.appendChild(table);
+});
