e005673bd5ff8d7c92dcb98387aaecddb79e6608
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-06-07 18:06:53 +0000

    Finally done, What a typeScript

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,3 +1,18 @@
 interface MajorCredits {
-    const credits: number =
+    credits: number;
+    brand: "major"
+}
+
+interface MinorCredits {
+    credits: number;
+    brand: "minor"
+}
+
+const sumMajorCredits = (subject1: MajorCredits, subject2: MinorCredits) => {
+    return subject1.credits + subject2.credits
+}
+
+
+const sumMinorCredits = (subject1: MinorCredits, subject2: MinorCredits) => {
+    return subject1.credits + subject2.credits
 }
